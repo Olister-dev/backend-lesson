45308d22a69d9ea63495f0f07d35313a19752862
Olister-dev <olisterryd@gmail.com>
2021-03-17 00:57:36 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+//Modules - Encapsulate Code (only share minimum)
+//ConomJS, every file is module (by default)
+
+const name = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative");
+require("./7-mind-granade");
+
+// sayHi("susan");
+// sayHi(name.john);
+// sayHi(name.peter);
